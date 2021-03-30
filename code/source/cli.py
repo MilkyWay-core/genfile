cf9731a7e90089c140cf30be38d219a645fbc598
MilkyWay-core <vinogradov.e@indriver.com>
2021-03-30 17:17:34 +0900

    add in cli default args

--- code/source/cli.py
+++ code/source/cli.py
@@ -1,12 +1,25 @@
 import argparse
 from pathlib import Path
+from .assist import get_path
 
 class cli:
     def __init__(self):
+        local_path = get_path()
         arg_parser = argparse.ArgumentParser(description='Cli-cli !!!')
-        arg_parser.add_argument('template', type=open, help='path to template')
-        arg_parser.add_argument('result', type=Path, help='path to the folder with the result')
-        arg_parser.add_argument('env', type=str, help='env from the template')
+        arg_parser.add_argument('template', 
+                                type=open, 
+                                help='path to template'
+                               )
+        arg_parser.add_argument('result', 
+                                type=Path,
+                                nargs='?', 
+                                help='path to the folder with the result',
+                                default=f'{local_path}/artifact/',)
+        arg_parser.add_argument('env', 
+                                type=str, 
+                                nargs='?',
+                                help='env from the template',
+                                default='123')
         args = arg_parser.parse_args()
         for arg in args._get_kwargs():
             print(str(arg[0])+' - '+str(arg[1]))
